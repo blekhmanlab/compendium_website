53f23c25f9c6d205bca7527764e1b2468d59a6ce
Vincent Rubinetti <vince.rubinetti@gmail.com>
2023-05-02 16:43:08 -0400

    vite scaffold

---
+++ .eslintrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:import/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:prettier/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  plugins: ["react-refresh", "prettier"],
+  rules: {
+    "prettier/prettier": "warn",
+    "react-refresh/only-export-components": "warn",
+  },
+};
