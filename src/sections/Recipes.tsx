2c89f2a3514f91fdc54f935be7c346fb8cf26253
Vincent Rubinetti <vince.rubinetti@gmail.com>
2023-07-19 10:24:40 -0400

    add recipe titles

--- src/sections/Recipes.tsx
+++ src/sections/Recipes.tsx
@@ -1,9 +1,27 @@
 const Recipes = () => (
   <>
     <details>
-      <summary>
-        See samples filtered by <code>xxx</code>
-      </summary>
+      <summary>All samples from a single world region</summary>
+      <p>
+        Lorem ipsum <code>dolor sit amet</code>, consectetur{" "}
+        <a href="link">adipiscing</a> elit, sed do eiusmod tempor incididunt ut
+        labore et dolore magna aliqua.
+      </p>
+      <ol>
+        <li>Sed elementum tempus egestas sed sed risus pretium quam.</li>
+        <li>Viverra nibh cras pulvinar mattis nunc sed.</li>
+        <li>Ultrices in iaculis nunc sed augue lacus viverra vitae congue.</li>
+      </ol>
+      <p>
+        Tortor condimentum lacinia quis vel eros donec. Ullamcorper malesuada
+        proin libero nunc consequat interdum varius sit. Adipiscing elit ut
+        aliquam purus sit amet luctus venenatis. Pulvinar pellentesque habitant
+        morbi tristique senectus et.
+      </p>
+    </details>
+
+    <details>
+      <summary>All samples from a set of 4 BioProjects</summary>
       <p>
         Lorem ipsum <code>dolor sit amet</code>, consectetur{" "}
         <a href="link">adipiscing</a> elit, sed do eiusmod tempor incididunt ut
@@ -24,7 +42,7 @@ const Recipes = () => (
 
     <details>
       <summary>
-        See samples filtered by <code>xxx</code>
+        All samples with read counts consolidated at the order level
       </summary>
       <p>
         Lorem ipsum <code>dolor sit amet</code>, consectetur{" "}
@@ -45,9 +63,7 @@ const Recipes = () => (
     </details>
 
     <details>
-      <summary>
-        See samples filtered by <code>xxx</code>
-      </summary>
+      <summary>All samples with less than 10 percent Firmicutes</summary>
       <p>
         Lorem ipsum <code>dolor sit amet</code>, consectetur{" "}
         <a href="link">adipiscing</a> elit, sed do eiusmod tempor incididunt ut
