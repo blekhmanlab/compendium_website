1bc648e734014395f17905c4d9bfe3836fb376b6
Vincent Rubinetti <vince.rubinetti@gmail.com>
2023-08-16 22:34:12 -0400

    rework map, add touch handling

--- src/sections/Map.tsx
+++ src/sections/Map.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import * as d3 from "d3";
 import { Feature } from "geojson";
 import Placeholder from "@/components/Placeholder";
@@ -9,7 +9,7 @@ import { clamp } from "@/util/math";
 import classes from "./Map.module.css";
 
 /** svg dimensions */
-const width = 800;
+const width = 770;
 const height = 400;
 
 const byOptions = ["Country", "Region"];
@@ -23,10 +23,20 @@ const Map = ({ id = "map" }) => {
 
   /** local state */
   const [by, setBy] = useState<By>(byOptions[0]);
+  const mapInstance = useRef<ReturnType<typeof map> | null>(null);
 
-  /** rerun d3 code when props change */
+  /** create unique instance of d3 map */
   useEffect(() => {
-    chart(id, by === "Country" ? byCountry : byRegion, selectedFeature);
+    mapInstance.current = map();
+  }, []);
+
+  /** update d3 map instance when props change */
+  useEffect(() => {
+    mapInstance.current?.(
+      id,
+      by === "Country" ? byCountry : byRegion,
+      selectedFeature,
+    );
   }, [id, byCountry, byRegion, by, selectedFeature]);
 
   /** show status */
@@ -65,201 +75,94 @@ const Map = ({ id = "map" }) => {
 export default Map;
 
 /** d3 code */
+const map = () => {
+  /** things that require one time setup per map instance */
 
-/** create projection */
-const projection = d3.geoNaturalEarth1();
+  /** create projection */
+  const projection = d3.geoNaturalEarth1();
 
-/** fit projection to bbox of earth */
-const fit = () =>
-  projection.fitSize([width, height], {
-    type: "Feature",
-    properties: {},
-    geometry: {
-      type: "Polygon",
-      coordinates: [
-        [
-          [-180, -90],
-          [180, -90],
-          [180, 90],
-          [-180, 90],
-        ],
-      ],
-    },
-  });
-fit();
+  fitProjection(projection);
 
-/** get scale when projection fit to earth bbox */
-const baseScale = projection.scale();
+  /** get scale when projection fit to earth bbox */
+  const baseScale = projection.scale();
 
-/** reset projection */
-const reset = () => {
-  projection.center([0, 0]);
-  projection.rotate([0, 0]);
-  projection.scale(baseScale);
-};
-reset();
-
-/** get mouse position in projection coordinates */
-const getMouse = (event: WheelEvent) => {
-  const [x = 0, y = 0] = projection.invert?.(d3.pointer(event)) || [];
-  return { x, y: -y };
-};
-
-/** path calculator for projection */
-const path = d3.geoPath().projection(projection);
-
-const graticules = d3.geoGraticule().step([20, 20])();
-
-const chart = (
-  id: string,
-  data: Data["byCountry"] | Data["byRegion"],
-  selectedFeature: Data["selectedFeature"],
-) => {
-  if (!data) return;
-
-  const svg = d3.select<SVGSVGElement, unknown>("#" + id);
-
-  /** draw projection outline */
-  svg
-    .select(".outline")
-    .selectAll("." + classes.outline)
-    .data([graticules])
-    .join("path")
-    .attr("class", classes.outline)
-    .attr("d", () => path({ type: "Sphere" }));
-
-  /** draw graticules */
-  svg
-    .select(".graticules")
-    .selectAll("." + classes.graticule)
-    .data([graticules])
-    .join("path")
-    .attr("class", classes.graticule)
-    .attr("d", path);
-
-  /** get range of sample counts */
-  const [, max = 1000] = d3.extent(data.features, (d) => d.properties.samples);
-
-  /** get css variable colors */
-  const primary = getCssVariable("--primary");
-  const gray = getCssVariable("--gray");
-  const secondary = getCssVariable("--secondary");
-  const darkGray = getCssVariable("--dark-gray");
-
-  /** color scale */
-  const scale = d3
-    .scaleLog<string>()
-    .domain([1, max])
-    .range([gray, primary])
-    .interpolate(d3.interpolateLab);
-
-  /** select country or region on mouse or key click */
-  const selectFeature = (
-    event: PointerEvent | KeyboardEvent,
-    d: (typeof data)["features"][number],
-  ) => {
-    const feature = d.properties;
-    /** key press */
-    if ("key" in event) {
-      if (event.key === "Enter") setSelectedFeature(feature);
-      if (event.key === "Escape") setSelectedFeature(undefined);
-    } else {
-      /** mouse click */
-      setSelectedFeature(feature);
-      event.stopPropagation();
-    }
+  /** reset projection */
+  const resetProjection = () => {
+    projection.center([0, 0]);
+    projection.rotate([0, 0]);
+    projection.scale(baseScale);
   };
+  resetProjection();
 
-  /** draw features */
-  svg
-    .select(".features")
-    .selectAll("." + classes.feature)
-    .data(data.features)
-    .join("path")
-    .attr("class", classes.feature)
-    .attr("d", path)
-    .attr("fill", (d) =>
-      !selectedFeature
-        ? scale(d.properties.samples || 1)
-        : (
-            selectedFeature.country
-              ? selectedFeature.country === d.properties.country
-              : selectedFeature.region === d.properties.region
-          )
-        ? secondary
-        : darkGray,
-    )
-    .attr("role", "graphics-symbol")
-    .attr(
-      "data-tooltip",
-      ({ properties: { region, country, code, samples } }) =>
-        [
-          `<div class="tooltip-table">`,
-          region && `<span>Region</span><span>${region || "???"}</span>`,
-          (country || code) &&
-            `<span>Country</span><span>${country || "???"} (${
-              code || "??"
-            })</span>`,
-          `<span>Samples</span><span>${samples.toLocaleString()}</span>`,
-          `</div>`,
-        ]
-          .filter(Boolean)
-          .join(""),
-    )
-    .on("keydown", selectFeature)
-    .on("click", selectFeature);
+  /** path calculator for projection */
+  const path = d3.geoPath().projection(projection);
 
-  type DragEvent = d3.D3DragEvent<Element, Data, unknown>;
+  /** long/lat lines */
+  const graticules = d3.geoGraticule().step([20, 20])();
 
-  /** move map view pan and zoom */
-  const moveView = (event?: DragEvent | WheelEvent) => {
+  /** d3 svg selection */
+  type SVG = d3.Selection<SVGSVGElement, unknown, HTMLElement, unknown>;
+
+  /** d3 zoom event */
+  type ZoomEvent = d3.D3ZoomEvent<SVGSVGElement, unknown>;
+
+  /** keep track of old transform to calculate deltas */
+  let oldTransform: d3.ZoomTransform | undefined;
+
+  /** update map view pan and zoom */
+  const updateMap = (svg: SVG, fullEvent?: ZoomEvent) => {
+    const { sourceEvent: event, transform } = fullEvent || {};
+
     /** get current projection components */
     let [x, y] = projection.center();
     let scale = projection.scale();
     let [lambda, phi] = projection.rotate();
 
-    /** update components based on event/transform */
-    if (event) {
-      if ("deltaY" in event) {
-        /** zoom event */
+    /** update components based on event transform */
+    if (event && transform && oldTransform) {
+      /** calculate deltas */
+      const dx = transform.x - oldTransform.x;
+      const dy = transform.y - oldTransform.y;
+      const dk = transform.k - oldTransform.k;
 
-        /** prevent page scroll */
-        event.preventDefault();
+      /** zoom event */
+      if (dk) {
         /** set new zoom */
-        scale = event.deltaY < 0 ? scale * 1.1 : scale / 1.1;
-        /** limit zoom */
-        scale = clamp(scale, baseScale, baseScale * 10);
+        scale = transform.k;
 
-        /** original coords of mouse */
-        const oldMouse = getMouse(event);
+        /** original coords of pointer */
+        const oldPointer = getPointer(svg.node(), projection, event);
 
         /** apply zoom */
         projection.scale(scale);
 
         /**
-         * iteratively pan map under mouse such that new mouse coords approach
-         * original
+         * iteratively pan map under pointer such that new pointer coords
+         * approach original
          */
         for (let iterations = 0; iterations < 3; iterations++) {
-          /** new coords of mouse */
-          const newMouse = getMouse(event);
+          /** new coords of pointer */
+          const newPointer = getPointer(svg.node(), projection, event);
 
           /** set new pan */
-          lambda += newMouse.x - oldMouse.x;
-          y += newMouse.y - oldMouse.y;
+          lambda += newPointer.x - oldPointer.x;
+          y += newPointer.y - oldPointer.y;
 
-          /** update pan */
+          /** apply pan */
           projection.rotate([lambda, phi]);
           projection.center([x, y]);
         }
       } else {
-        /** drag event */
+        /** pan event */
 
         /** set new pan */
-        lambda += (baseScale / 2) * (event.dx / scale);
-        y += (baseScale / 2) * (event.dy / scale);
+        lambda += (baseScale / scale / 2) * dx;
+        y += (baseScale / scale / 2) * dy;
       }
     }
+
+    /** store old transform */
+    if (transform) oldTransform = transform;
 
     /** limit pan */
     const yLimit = 0.89 * (90 - 90 * (baseScale / scale));
@@ -279,23 +182,186 @@ const chart = (
     svg.selectAll<Element, Feature>("." + classes.feature).attr("d", path);
   };
 
-  /** drag handler */
-  const drag = d3.drag<SVGSVGElement, unknown, unknown>().on("drag", moveView);
+  /** return func to run for updating map */
+  return (
+    id: string,
+    data: Data["byCountry"] | Data["byRegion"],
+    selectedFeature: Data["selectedFeature"],
+  ) => {
+    if (!data) return;
 
-  /** connect drag handler to svg */
-  svg.call(drag);
+    /** get svg selection */
+    const svg = d3.select<SVGSVGElement, unknown>("#" + id);
 
-  /** zoom handler */
-  svg.on("wheel", (event: WheelEvent) => moveView(event));
+    if (!svg.node()) return;
 
-  /** double click handler */
-  svg.on("dblclick.zoom", () => {
-    reset();
-    moveView();
-  });
+    /** draw projection outline */
+    svg
+      .select(".outline")
+      .selectAll("." + classes.outline)
+      .data([graticules])
+      .join("path")
+      .attr("class", classes.outline)
+      .attr("d", () => path({ type: "Sphere" }));
 
-  /** unset selected feature when clicking off map */
-  d3.select(window).on("click", () => {
-    setSelectedFeature();
-  });
+    /** draw graticules */
+    svg
+      .select(".graticules")
+      .selectAll("." + classes.graticule)
+      .data([graticules])
+      .join("path")
+      .attr("class", classes.graticule)
+      .attr("d", path);
+
+    /** get range of sample counts */
+    const [, max = 1000] = d3.extent(
+      data.features,
+      (d) => d.properties.samples,
+    );
+
+    /** get css variable colors */
+    const primary = getCssVariable("--primary");
+    const gray = getCssVariable("--gray");
+    const secondary = getCssVariable("--secondary");
+    const darkGray = getCssVariable("--dark-gray");
+
+    /** color scale */
+    const scale = d3
+      .scaleLog<string>()
+      .domain([1, max])
+      .range([gray, primary])
+      .interpolate(d3.interpolateLab);
+
+    /** draw features */
+    svg
+      .select(".features")
+      .selectAll("." + classes.feature)
+      .data(data.features)
+      .join("path")
+      .attr("class", classes.feature)
+      .attr("d", path)
+      .attr("fill", (d) =>
+        !selectedFeature
+          ? scale(d.properties.samples || 1)
+          : (
+              selectedFeature.country
+                ? selectedFeature.country === d.properties.country
+                : selectedFeature.region === d.properties.region
+            )
+          ? secondary
+          : darkGray,
+      )
+      .attr("role", "graphics-symbol")
+      .attr(
+        "data-tooltip",
+        ({ properties: { region, country, code, samples } }) =>
+          [
+            `<div class="tooltip-table">`,
+            region && `<span>Region</span><span>${region || "???"}</span>`,
+            (country || code) &&
+              `<span>Country</span><span>${country || "???"} (${
+                code || "??"
+              })</span>`,
+            `<span>Samples</span><span>${samples.toLocaleString()}</span>`,
+            `</div>`,
+          ]
+            .filter(Boolean)
+            .join(""),
+      )
+      .on("keydown", selectFeature)
+      .on("click", selectFeature);
+
+    /** zoom handler */
+    const zoom = d3
+      .zoom<SVGSVGElement, unknown>()
+      .scaleExtent([baseScale, baseScale * 10])
+      .on("zoom", (event) => updateMap(svg, event));
+
+    /** connect zoom handler to svg */
+    svg
+      .call((event) => zoom(svg, event))
+      /** always prevent scroll on wheel, not just when at scale limit */
+      .on("wheel", (event) => event.preventDefault());
+
+    /** double click handler */
+    svg.on("dblclick.zoom", () => {
+      zoom.transform(svg, d3.zoomIdentity);
+      resetProjection();
+      updateMap(svg);
+    });
+  };
 };
+
+/** fit projection to bbox of earth */
+const fitProjection = (projection: d3.GeoProjection) =>
+  projection.fitSize([width, height], {
+    type: "Feature",
+    properties: {},
+    geometry: {
+      type: "Polygon",
+      coordinates: [
+        [
+          [-180, -90],
+          [180, -90],
+          [180, 90],
+          [-180, 90],
+        ],
+      ],
+    },
+  });
+
+/** get pointer position in projection coordinates */
+const getPointer = (
+  svg: SVGSVGElement | null,
+  projection: d3.GeoProjection,
+  event: PointerEvent | WheelEvent | TouchEvent,
+) => {
+  /** point in screen coords */
+  const screenPoint = new DOMPoint(0, 0);
+
+  /** mouse */
+  if ("clientX" in event) {
+    screenPoint.x = event.clientX;
+    screenPoint.y = event.clientY;
+  }
+
+  /** touch(es) */
+  if ("touches" in event) {
+    if (event.touches.length === 1) {
+      screenPoint.x = event.touches[0].clientX;
+      screenPoint.y = event.touches[0].clientY;
+    }
+    if (event.touches.length === 2) {
+      screenPoint.x = (event.touches[0].clientX + event.touches[1].clientX) / 2;
+      screenPoint.y = (event.touches[0].clientY + event.touches[1].clientY) / 2;
+    }
+  }
+
+  /** point in svg coords */
+  const svgPoint = screenPoint.matrixTransform(svg?.getScreenCTM()?.inverse());
+
+  /** point in map coords */
+  const mapPoint = projection.invert?.([svgPoint.x, svgPoint.y]) || [];
+
+  return { x: mapPoint[0] || 0, y: -(mapPoint[1] || 0) };
+};
+
+/** select country or region on pointer or key click */
+const selectFeature = (
+  event: PointerEvent | KeyboardEvent,
+  d: NonNullable<Data["byCountry"] | Data["byRegion"]>["features"][number],
+) => {
+  const feature = d.properties;
+  /** key press */
+  if ("key" in event) {
+    if (event.key === "Enter") setSelectedFeature(feature);
+    if (event.key === "Escape") setSelectedFeature(undefined);
+  } else {
+    /** pointer click */
+    setSelectedFeature(feature);
+    event.stopPropagation();
+  }
+};
+
+/** unset selected feature when clicking off map */
+d3.select(window).on("click", () => setSelectedFeature());
