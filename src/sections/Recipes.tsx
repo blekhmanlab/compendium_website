3cf73a195039ab938bbd009310f062ec81196997
Vincent Rubinetti <vince.rubinetti@gmail.com>
2023-08-29 15:42:34 -0600

    add real recipe links

--- src/sections/Recipes.tsx
+++ src/sections/Recipes.tsx
@@ -3,6 +3,29 @@ import { ReactComponent as RecipeIcon } from "@/assets/recipe.svg";
 import Button from "@/components/Button";
 import classes from "./Recipes.module.css";
 
+const recipes = [
+  {
+    link: "samples-in-specific-bioprojects",
+    name: "Get samples in specific BioProjects",
+  },
+  {
+    link: "samples-from-a-specific-world-region",
+    name: "Get samples from a specific world region",
+  },
+  {
+    link: "histogram-of-total-bases-per-sample",
+    name: "See a histogram of total bases per sample",
+  },
+  {
+    link: "read-counts-aggregated-to-order",
+    name: "See read counts aggregated to order",
+  },
+  {
+    link: "samples-filtered-by-presence-of-microbe",
+    name: "Get samples filtered by presence of microbe",
+  },
+];
+
 const Recipes = () => (
   <section>
     <h2>Recipes</h2>
@@ -16,30 +39,17 @@ const Recipes = () => (
     </p>
 
     <div className={classes.buttons}>
-      <Button
-        icon={RecipeIcon}
-        href={import.meta.env.VITE_R_PACKAGE + "#recipe-1"}
-      >
-        Get samples from a single world region
-      </Button>
-      <Button
-        icon={RecipeIcon}
-        href={import.meta.env.VITE_R_PACKAGE + "#recipe-2"}
-      >
-        Get samples from a set of 4 BioProjects
-      </Button>
-      <Button
-        icon={RecipeIcon}
-        href={import.meta.env.VITE_R_PACKAGE + "#recipe-3"}
-      >
-        Get samples with read counts consolidated at the order level
-      </Button>
-      <Button
-        icon={RecipeIcon}
-        href={import.meta.env.VITE_R_PACKAGE + "#recipe-4"}
-      >
-        Get samples with less than 10% Firmicutes
-      </Button>
+      {recipes.map(({ link, name }, index) => (
+        <Button
+          key={index}
+          icon={RecipeIcon}
+          href={
+            import.meta.env.VITE_R_PACKAGE + "articles/overview.html#" + link
+          }
+        >
+          {name}
+        </Button>
+      ))}
     </div>
   </section>
 );
