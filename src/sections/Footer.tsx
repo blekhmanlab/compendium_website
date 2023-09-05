48fd97b365931b814c6217350f0c648bc97e97e3
Vincent Rubinetti <vince.rubinetti@gmail.com>
2023-09-05 15:19:54 -0400

    add univ logos

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -2,53 +2,61 @@ import classes from "./Footer.module.css";
 
 const Footer = () => (
   <footer>
-    <div className={classes.footer}>
-      <div>
-        <h3>Affiliations</h3>
-
-        <a
-          href="https://medicine.uchicago.edu/sections/genetic-medicine/"
-          target="_blank"
-        >
-          Univ. of Chicago | Section of Genetic Medicine
-        </a>
-        <br />
-        <a href="https://medschool.cuanschutz.edu/dbmi" target="_blank">
-          Univ. of Colorado | Dept. of Biomedical Informatics
-        </a>
-        <br />
-        <a href="https://cbs.umn.edu/gcd" target="_blank">
-          Univ. of Minnesota | Dept. of GCD
-        </a>
-      </div>
-
-      <div>
-        <h3>Collaborators</h3>
-
-        <a href="http://blekhmanlab.org/" target="_blank">
-          Blekhman Lab
-        </a>
-        <br />
-        <a href="http://greenelab.com/" target="_blank">
-          Greene Lab
-        </a>
-      </div>
-
-      <div>
-        <h3>Source Code</h3>
-
+    <div className="sub-section">
+      <p>
+        Source code: &nbsp;
         <a href="link" target="_blank">
           Database
-        </a>
-        <br />
+        </a>{" "}
+        &nbsp;
         <a href="link" target="_blank">
           R Package
-        </a>
-        <br />
+        </a>{" "}
+        &nbsp;
         <a href="link" target="_blank">
           Scripts
         </a>
-      </div>
+      </p>
+
+      <p>
+        A project of the{" "}
+        <a href="http://blekhmanlab.org/" target="_blank">
+          Blekhman Lab
+        </a>{" "}
+        and{" "}
+        <a href="http://greenelab.com/" target="_blank">
+          Greene Lab
+        </a>
+        , in affiliation with...
+      </p>
+    </div>
+
+    <div className={classes.cols}>
+      <a
+        href="https://medicine.uchicago.edu/sections/genetic-medicine/"
+        target="_blank"
+        data-tooltip="University of Chicago | Section of Genetic Medicine"
+        className={classes.logo}
+      >
+        <img src="chicago.png" alt="University of Chicago logo" />
+      </a>
+      <a
+        href="https://medschool.cuanschutz.edu/dbmi"
+        target="_blank"
+        data-tooltip="University of Colorado | Department of Biomedical Informatics"
+        className={classes.logo}
+      >
+        <img src="colorado.png" alt="University of Colorado logo" />
+      </a>
+      <a
+        href="https://cbs.umn.edu/gcd"
+        target="_blank"
+        data-tooltip="University of Minnesota | Department of Genetics, Cell Biology, and
+        Development"
+        className={classes.logo}
+      >
+        <img src="minnesota.png" alt="University of Minnesota logo" />
+      </a>
     </div>
   </footer>
 );
