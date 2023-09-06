39552c816ac35b67c0bc7f152f10bf77652ba779
Vincent Rubinetti <vince.rubinetti@gmail.com>
2023-09-06 10:25:45 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.GITHUB_ACTIONS ? "/compendium_website/" : "/",
+  base: "/",
   plugins: [react(), svgr()],
   resolve: {
     alias: {
