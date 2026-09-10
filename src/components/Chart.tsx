ac023c0a1ba18b51c4f64ee68a1e0d9aca3b1882
Vincent Rubinetti <vince.rubinetti@gmail.com>
2026-09-10 17:55:55 -0400

    crop download

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -2,11 +2,12 @@ import type { ECharts, EChartsInitOpts, EChartsOption } from "echarts";
 import { useEffect, useEffectEvent, useId, useRef, useState } from "react";
 import { useDebounceFn, useResizeObserver } from "@reactuses/core";
 import clsx from "clsx";
-import { toPng } from "dom-to-image-more";
+import { toCanvas } from "dom-to-image-more";
 import { connect, init, registerTheme } from "echarts";
 import { DownloadIcon } from "lucide-react";
 import { sleep } from "@/util/async";
 import { getCssVariable } from "@/util/dom";
+import { range, clamp } from "lodash";
 
 type Props = {
   option: EChartsOption;
@@ -29,6 +30,7 @@ export default function Chart({
   const id = useId();
   const [ref, setRef] = useState<HTMLDivElement | null>(null);
   const chart = useRef<ECharts>(null);
+  const [downloading, setDownloading] = useState(false);
 
   /** get latest values without re-running effect */
   const getInitOptions = useEffectEvent(() => initOptions);
@@ -93,14 +95,81 @@ export default function Chart({
       <button
         className="absolute top-[anchor(top)] right-[anchor(right)] z-10 size-8 rounded-md hover:bg-gray"
         style={{ positionAnchor: `--${id}` }}
+        disabled={downloading}
         onClick={async () => {
           if (!ref) return;
+
+          setDownloading(true);
+
+          /** options */
+          const scale = 4;
+          const padding = 16 * scale;
+
+          /** render */
           const element = downloadElement(ref);
-          const url = await toPng(element, { scale: 4 });
-          const a = document.createElement("a");
-          a.href = url;
-          a.download = download;
-          a.click();
+          // eslint-disable-next-line
+          ref.style.resize = "none";
+          const canvas = await toCanvas(element, { scale });
+          ref.style.resize = "";
+
+          /** hide resize handle */
+
+          /** access pixel data */
+          let ctx = canvas.getContext("2d")!;
+          const pixels = ctx.getImageData(0, 0, canvas.width, canvas.height);
+          const getPixel = (x: number, y: number) =>
+            pixels.data[(y * canvas.width + x) * 4 + 3];
+
+          /** find crop edge */
+          const getEdge = (ys: number[], xs: number[], swap = false) => {
+            for (const y of ys)
+              for (const x of xs)
+                if (swap ? getPixel(y, x) : getPixel(x, y)) return y;
+            return 0;
+          };
+
+          /** coords */
+          const ys = range(0, canvas.height);
+          const xs = range(0, canvas.width);
+
+          /** find all crop edges */
+          let top = getEdge(ys, xs);
+          let bottom = getEdge(ys.toReversed(), xs);
+          let left = getEdge(xs, ys, true);
+          let right = getEdge(xs.toReversed(), ys, true);
+
+          /** add padding */
+          top -= padding;
+          bottom += padding;
+          left -= padding;
+          right += padding;
+
+          /** clamp */
+          top = clamp(top, 0, canvas.height / 2);
+          bottom = clamp(bottom, canvas.height / 2, canvas.height);
+          left = clamp(left, 0, canvas.width / 2);
+          right = clamp(right, canvas.width / 2, canvas.width);
+
+          /** calc size */
+          const width = right - left + 1;
+          const height = bottom - top + 1;
+
+          /** create new cropped canvas */
+          const cropped = document.createElement("canvas");
+          cropped.width = width;
+          cropped.height = height;
+          ctx = cropped.getContext("2d")!;
+
+          /** draw cropped image onto new canvas */
+          ctx.drawImage(canvas, left, top, width, height, 0, 0, width, height);
+
+          /** download */
+          const link = document.createElement("a");
+          link.download = download;
+          link.href = cropped.toDataURL("image/png");
+          link.click();
+
+          setDownloading(false);
         }}
         aria-label="Download chart"
       >
